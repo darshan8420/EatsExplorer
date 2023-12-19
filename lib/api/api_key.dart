745f2b005fb517a57fd73a16099f87254da82f87
Sudarshan Tiwari <151531786+darshan8420@users.noreply.github.com>
2023-12-19 17:57:29 +0100

    Delete lib directory

--- lib/api/api_key.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:math';
-
-class ApiKey {
-  static String keys = '87eb68a522d846b082cd48d2052ceb98';
-}
--- lib/screens/home_screen/bloc/homerecipes_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'homerecipes_bloc.dart';
-
-@immutable
-abstract class HomeRecipesEvent {}
-
-class LoadHomeRecipe extends HomeRecipesEvent {}
